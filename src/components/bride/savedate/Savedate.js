8e04de7f251f170e6a1393b1d56b4b5005b09242
2amlover <126642452+2amlover@users.noreply.github.com>
2023-10-11 15:16:38 +0530

    v2 sd

--- src/components/bride/savedate/Savedate.js
+++ src/components/bride/savedate/Savedate.js
@@ -3,7 +3,7 @@ import React from "react";
 const SaveDate = () => {
   const weddingDate = "October 14, 2023"; // Your wedding date
 
-  const addEventToCalendar = () => {
+  const addEventToGoogleCalendar = () => {
     const event = {
       start: new Date('2023-10-14T00:00:00Z'), // Replace with your actual start date
       end: new Date('2023-10-15T23:59:59Z'), // Replace with your actual end date
@@ -12,12 +12,18 @@ const SaveDate = () => {
       location: "Venue Name",
     };
 
-    const startDateStr = event.start.toISOString().replace(/-|:|\.\d+/g, "");
-    const endDateStr = event.end.toISOString().replace(/-|:|\.\d+/g, "");
+    const gapi = window.gapi; // Assuming that you've already loaded the Google API client
 
-    const calendarUrl = `webcal://example.com?start=${startDateStr}&end=${endDateStr}&summary=${encodeURIComponent(event.summary)}&description=${encodeURIComponent(event.description)}&location=${encodeURIComponent(event.location)}`;
+    gapi.auth2.getAuthInstance().signIn().then(() => {
+      const request = gapi.client.calendar.events.insert({
+        calendarId: 'primary', // 'primary' is the default calendar for the authenticated user
+        resource: event,
+      });
 
-    window.open(calendarUrl, '_blank');
+      request.execute((response) => {
+        console.log('Event added:', response);
+      });
+    });
   };
 
   return (
@@ -27,8 +33,8 @@ const SaveDate = () => {
         <div className="subtext">We're getting married</div>
         <div className="subtext1">Save the date</div>
         <div className="date">{weddingDate}</div>
-        <button className="button" onClick={addEventToCalendar}>
-          Save Date to Calendar
+        <button className="button" onClick={addEventToGoogleCalendar}>
+          Save Date to Google Calendar
         </button>
       </div>
     </div>
