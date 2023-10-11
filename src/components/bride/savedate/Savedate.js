4bca7de154a9896d4fbe51650ad65197ed0736fe
2amlover <126642452+2amlover@users.noreply.github.com>
2023-10-11 15:21:42 +0530

    v3 sd

--- src/components/bride/savedate/Savedate.js
+++ src/components/bride/savedate/Savedate.js
@@ -1,41 +1,58 @@
 import React from "react";
+import backgroundImage from "../../../assets/savedate-bg.png";
 
 const SaveDate = () => {
   const weddingDate = "October 14, 2023"; // Your wedding date
 
-  const addEventToGoogleCalendar = () => {
-    const event = {
-      start: new Date('2023-10-14T00:00:00Z'), // Replace with your actual start date
-      end: new Date('2023-10-15T23:59:59Z'), // Replace with your actual end date
-      summary: "Our Wedding",
-      description: "Jhon X Deo's Wedding",
-      location: "Venue Name",
-    };
+  const generateICSFile = () => {
+    const icsData = `BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//Example Corp.//iCal 4.0//EN
+CALSCALE:GREGORIAN
+METHOD:PUBLISH
+BEGIN:VEVENT
+UID:1@example.com
+DTSTAMP:20231014T000000Z
+DTSTART:20231014T000000Z
+DTEND:20231015T235959Z
+SUMMARY:Our Wedding
+DESCRIPTION:Jhon X Deo's Wedding
+LOCATION:Venue Name
+END:VEVENT
+END:VCALENDAR`;
 
-    const gapi = window.gapi; // Assuming that you've already loaded the Google API client
-
-    gapi.auth2.getAuthInstance().signIn().then(() => {
-      const request = gapi.client.calendar.events.insert({
-        calendarId: 'primary', // 'primary' is the default calendar for the authenticated user
-        resource: event,
-      });
-
-      request.execute((response) => {
-        console.log('Event added:', response);
-      });
-    });
+    const blob = new Blob([icsData], { type: 'text/calendar;charset=utf-8' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'WeddingDate.ics';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
   };
 
   return (
     <div className="overlayyy">
-      <div className="save-date-container">
-        <div className="names">Jhon X Deo</div>
-        <div className="subtext">We're getting married</div>
-        <div className="subtext1">Save the date</div>
-        <div className="date">{weddingDate}</div>
-        <button className="button" onClick={addEventToGoogleCalendar}>
-          Save Date to Google Calendar
-        </button>
+      <div
+        style={{
+          backgroundImage: `url(${backgroundImage})`, // Use the imported image
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          position: "relative",
+          filter: "blur(0px)",
+          zIndex: -1,
+          borderRadius: "20px",
+        }}
+      >
+        <div className="save-date-container">
+          <div className="names">Jhon X Deo</div>
+          <div className="subtext">We're getting married</div>
+          <div className="subtext1">Save the date</div>
+          <div className="date">{weddingDate}</div>
+          <button className="button" onClick={generateICSFile}>
+            Save Date
+          </button>
+        </div>
       </div>
     </div>
   );
