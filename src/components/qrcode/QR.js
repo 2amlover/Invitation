32230756294f1bc2468e8e190e6f0e3905ee681f
2amlover <126642452+2amlover@users.noreply.github.com>
2023-10-11 14:35:17 +0530

    updated qr

--- src/components/qrcode/QR.js
+++ src/components/qrcode/QR.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import QRCodeReact from "qrcode.react";
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const QR = () => {
+  // The complete URL of the content page you want to open
+  const contentPageURL = "https://invite-2amlover.vercel.app/content"; // Replace with your actual URL
+  // You can encode the contentPageURL directly into the QR code
+  const qrCodeContent = contentPageURL;
+
   return (
     <div className="container mt-5">
       <div className="qr-code">
-        <QRCodeReact />
+        <QRCodeReact value={qrCodeContent} />
       </div>
       <div className="open-button mt-4">
-        <Link to="/content" className="btn btn-primary px-5">
+        {/* The Link component should point to the content page */}
+        <Link to={contentPageURL} className="btn btn-primary px-5">
           Scan me
         </Link>
       </div>
